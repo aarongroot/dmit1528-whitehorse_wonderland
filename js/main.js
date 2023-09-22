49a4f9b745badad51cbf4d85324f9bdbb67b1d22
unknown <potatopicker963@gmail.com>
2023-09-22 09:08:34 -0600

    Added more content with formatting

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ const btn = document.querySelector('.toggle-btn');
 btn.addEventListener('click', (evt) => {
     document.querySelector('nav').classList.toggle('show-nav');
 });
+
+const loadMore = document.querySelector('.load-more');
+loadMore.addEventListener('click', (evt) => {
+    document.querySelector('.events-flex').classList.toggle('show-events');
+    document.querySelector('.load-more').classList.toggle('hide-btn');
+});
+
+
+
+
+
+
